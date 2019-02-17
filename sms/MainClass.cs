849bff82571dd59a3a36ca17c59e83575dfe24bd
Abdul Rafay Modi <rafaymodi18@gmail.com>
2019-02-17 15:11:00 +0500

    Disable_ENABLE_RESET methods

--- sms/MainClass.cs
+++ sms/MainClass.cs
@@ -28,5 +28,175 @@ namespace sms
                 return MessageBox.Show(msg, heading, MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+        public static void disable_reset(Panel p)
+        {
+            foreach (Control c in p.Controls)
+            {
+                if (c is TextBox)
+                {
+                    TextBox t = (TextBox)c;
+                    t.Enabled = false;
+                    t.Text = "";
+                }
+                if (c is ComboBox)
+                {
+                    ComboBox cb = (ComboBox)c;
+                    cb.Enabled = false;
+                    cb.SelectedIndex = -1;
+                }
+                if (c is RadioButton)
+                {
+                    RadioButton rb = (RadioButton)c;
+                    rb.Enabled = false;
+                    rb.Checked = false;
+                }
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    cb.Enabled = false;
+                    cb.Checked = false;
+                }
+                if (c is DateTimePicker)
+                {
+                    DateTimePicker dtp = (DateTimePicker)c;
+                    dtp.Enabled = false;
+                    dtp.Value = DateTime.Now;
+                }
+            }
+        }
+        public static void disable(Panel p)
+        {
+            foreach (Control c in p.Controls)
+            {
+                if (c is TextBox)
+                {
+                    TextBox t = (TextBox)c;
+                    t.Enabled = false;
+                }
+                if (c is ComboBox)
+                {
+                    ComboBox cb = (ComboBox)c;
+                    cb.Enabled = false;
+                }
+                if (c is RadioButton)
+                {
+                    RadioButton rb = (RadioButton)c;
+                    rb.Enabled = false;
+                }
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    cb.Enabled = false;
+                }
+                if (c is DateTimePicker)
+                {
+                    DateTimePicker dtp = (DateTimePicker)c;
+                    dtp.Enabled = false;
+                }
+            }
+        }
+        public static void enable_reset(Panel p)
+        {
+            foreach (Control c in p.Controls)
+            {
+                if (c is TextBox)
+                {
+                    TextBox t = (TextBox)c;
+                    t.Enabled = true;
+                    t.Text = "";
+                }
+                if (c is ComboBox)
+                {
+                    ComboBox cb = (ComboBox)c;
+                    cb.Enabled = true;
+                    cb.SelectedIndex = -1;
+                }
+                if (c is RadioButton)
+                {
+                    RadioButton rb = (RadioButton)c;
+                    rb.Enabled = true;
+                    rb.Checked = false;
+                }
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    cb.Enabled = true;
+                    cb.Checked = false;
+                }
+                if (c is DateTimePicker)
+                {
+                    DateTimePicker dtp = (DateTimePicker)c;
+                    dtp.Enabled = true;
+                    dtp.Value = DateTime.Now;
+                }
+            }
+        }
+        public static void enable_reset(GroupBox gb)
+        {
+            foreach (Control c in gb.Controls)
+            {
+                if (c is TextBox)
+                {
+                    TextBox t = (TextBox)c;
+                    t.Enabled = true;
+                    t.Text = "";
+                }
+                if (c is ComboBox)
+                {
+                    ComboBox cb = (ComboBox)c;
+                    cb.Enabled = true;
+                    cb.SelectedIndex = -1;
+                }
+                if (c is RadioButton)
+                {
+                    RadioButton rb = (RadioButton)c;
+                    rb.Enabled = true;
+                    rb.Checked = false;
+                }
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    cb.Enabled = true;
+                    cb.Checked = false;
+                }
+                if (c is DateTimePicker)
+                {
+                    DateTimePicker dtp = (DateTimePicker)c;
+                    dtp.Enabled = true;
+                    dtp.Value = DateTime.Now;
+                }
+            }
+        }
+        public static void enable(Panel p)
+        {
+            foreach (Control c in p.Controls)
+            {
+                if (c is TextBox)
+                {
+                    TextBox t = (TextBox)c;
+                    t.Enabled = true;
+                }
+                if (c is ComboBox)
+                {
+                    ComboBox cb = (ComboBox)c;
+                    cb.Enabled = true;
+                }
+                if (c is RadioButton)
+                {
+                    RadioButton rb = (RadioButton)c;
+                    rb.Enabled = true;
+                }
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    cb.Enabled = true;
+                }
+                if (c is DateTimePicker)
+                {
+                    DateTimePicker dtp = (DateTimePicker)c;
+                    dtp.Enabled = true;
+                }
+            }
+        }
     }
 }
